31efb226dccd560daf9b312815cfcdf81ea893de
Ustinov.vyacheslav <vustinov@fciit.ru>
2023-10-09 17:02:14 +0300

    add-docker fix

--- gateway/src/main/java/ru/practicum/shareit/item/client/ItemClient.java
+++ gateway/src/main/java/ru/practicum/shareit/item/client/ItemClient.java
@@ -37,7 +37,7 @@ public class ItemClient extends BaseClient {
 
     public ResponseEntity<Object> search(String text, Integer from, Integer size) {
         Map<String, Object> parameters = Map.of("text", text, "from", from, "size", size);
-        return get("/search?text={text}&from={from}&size={size}", parameters);
+        return get("/search?text={text}&from={from}&size={size}", null, parameters);
     }
 
     public ResponseEntity<Object> create(Long userId, ItemDto itemRequestDto) {
